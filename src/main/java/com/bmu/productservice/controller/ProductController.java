7f3cceecc47c5c46d095926259b15b31895c0cca
mubinbin <dinobunmu@gmail.com>
2023-04-19 16:11:54 -0500

    created ProductController, ProductRequest, and ProductService

---
+++ src/main/java/com/bmu/productservice/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.bmu.productservice.controller;
+
+import com.bmu.productservice.dto.ProductRequest;
+import com.bmu.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+}
